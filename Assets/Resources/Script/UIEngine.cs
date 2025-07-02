5aee8b361a84e5f0a406997a0f164f5a82fe4869
Ramzisyuhada <ramzi.syuhada@mhs.itenas.ac.id>
2025-07-02 09:55:19 +0700

    UI Menu

---
+++ Assets/Resources/Script/UIEngine.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIEngine : MonoBehaviour
+{
+    public void Materi()
+    {
+        SceneManager.LoadScene("Halaman Pilihan Materi");
+    }
+
+    public void Mulai()
+    {
+        SceneManager.LoadScene("Home Screen");
+    }
+
+    public void BangunDatar()
+    {
+        SceneManager.LoadScene("Halaman Materi ruang sisi datar");
+    }
+}
